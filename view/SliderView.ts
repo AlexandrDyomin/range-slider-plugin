760fe0eeff6f7bcc976df52f2bbf27320d822c9c
Alexandr Dyomin <alexandr.dyomin@yahoo.com>
2022-02-20 18:27:36 +0900

    Изменил поля класса, метод _getTemplate

--- view/SliderView.ts
+++ view/SliderView.ts
@@ -1,44 +1,48 @@
+import type { sliderSettings } from "../factory/slider";
+
+
 interface ISliderView {
   updateValue(value: number): void;
 }
 
 
 class SliderView implements ISliderView {
-  private _template: string ;
-  private _roller: HTMLElement;
+  private _template: string; // html-шаблон слайдера
+  private _rollers: NodeList; // хранит бегунки
 
-  constructor(private _type:string, private _container: HTMLElement){
-    this._template = this._getTemplate();
-    this._render();
-    this._roller! = _container.querySelector(".roller")!;
+  constructor(
+    private _container: Element, // контейнер, в котором будет расположен слайдер
+    private _settings: sliderSettings // настройки слайдера
+    ) {
+      this._template = this._getTemplate();
+      this._render();
+      this._rollers = _container.querySelectorAll(".slider__roller");
   }
 
+  // вставляет шаблон слайдера в контейнер
   private _render(): void {
     this._container.innerHTML = this._template;
   }
 
+  // возвращает шаблон слайдера
   private _getTemplate(): string {
-    let modifier: string;
-    switch (this._type) {
-      case "vertical":
-        modifier = "slider__scale_vertical";
-        break;
-      case "circle":
-        modifier = "slider__scale_circle";
-        break;
-      default: modifier = "slider__scale_horizontal";
-    }
+    let { min, max, step, value, type, range } = this._settings;
 
     return `<div class="slider">
-      <div class="slider__scale ${modifier}">
-        <div class="slider__range"></div>
-        <div class="slider__roller"></div>
-      </div>
+      <input class="slider-default_hidden" type="range" min="${ min }" max="${ max }" step="${ step }" value="${ value }">
       <output class="slider__display"></output>
-      </div>`;
+      <div class="slider__scale slider__scale_${type}">
+        <div class="slider__range"></div>
+        <div class="slider__roller slider__roller_first"></div>
+        ${ range ? '<div class="slider__roller slider__roller_second"></div>': "" }
+      </div>
+    </div>`
   }
 
-  updateValue(value: number): void {
-
+  // обновляет отображение слайдера
+  updateValue(): void {
+    console.log("yoyoyo");
   }
 }
+
+export default SliderView;
