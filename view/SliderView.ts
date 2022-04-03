a75288ddabb50545c3dace04db0e3c3e05dc1bdf
Alexandr Dyomin <alexandr.dyomin@yahoo.com>
2022-04-03 23:21:54 +0900

    Рефакторинг

--- view/SliderView.ts
+++ view/SliderView.ts
@@ -1,4 +1,5 @@
 import type { sliderSettings } from "../Slider";
+import ATemplate from "./Template";
 
 
 interface ISliderView {
@@ -37,10 +38,13 @@ class SliderView implements ISliderView {
   private indexOfRoller: 0 | 1 | null = null;                   // индекс ролика, на котором совершен клик
   private defaultSlider: HTMLInputElement | null = null; // стандартный слайдер
 
+
+
+
   constructor(container: HTMLElement, settings: sliderSettings) {
     this.settings = settings;
     this.container = container;
-    this.template = this.getTemplate();
+    this.template = ATemplate.getTemplate(settings);
 
     // отобразим слайдер на странице
     this.container.innerHTML = this.template;
@@ -357,37 +361,6 @@ class SliderView implements ISliderView {
     // вычислим на какое количество шагов необходимо сдвинуть бегунок
     let steps: number = Math.round(offset / this.step);
     return steps;
-  }
-
-  // возвращает html-код слайдера
-  private getTemplate(): string {
-    let { min, max, step, value, values, type, range } = this.settings;
-
-    return (
-      `<div class="slider">
-        ${
-          range ?
-            `<input class="slider__min-val" value="${values![0]}">
-            <input class="slider__max-val" value="${values![1]}">` :
-            `<input class="slider-default_hidden"
-                    type="range" min="${ min }"
-                    max="${ max }"
-                    step="${ step }"
-                    value="${ value }">`
-        }
-
-        <output class="slider__display"></output>
-        <div class="slider__scale slider__scale_${ type }">
-          <div class="slider__range"></div>
-          ${
-            range ?
-              `<div class="slider__roller slider__roller_first"></div>
-              <div class="slider__roller slider__roller_second"></div>`:
-              `<div class="slider__roller slider__roller_first"></div>`
-          }
-        </div>
-      </div>`
-    );
   }
 
   private paintRange(): void {
--- view/Template.ts
+++ view/Template.ts
@@ -1,45 +1,32 @@
 import type { sliderSettings } from "../Slider";
 
 
-interface ITemplate {
-  getTemplate(): string;
-}
+abstract class ATemplate {
+  static getTemplate(settings: sliderSettings): string {
+    let { range, type, min, max, value, values, step } = settings;
 
-
-class Template implements ITemplate {
-  private settings: sliderSettings;
-
-  constructor(settings: sliderSettings) {
-    this.settings = settings;
-  }
-
-  public getTemplate(): string {
-    let { range, type, min, max, value, values, step } = this.settings;
-    
-    return ` 
+    return `
       <div class="slider">
         ${ range ?
-             `<input class="slider__min-val" value="${values![0]}">
-             <input class="slider__max-val" value="${values![1]}">` :
-             `<input class="slider-default_hidden"
-                     type="range" min="${ min }"
-                     max="${ max }"
-                     step="${ step }"
-                     value="${ value }">` }
+          `<input class="slider__min-val" value="${values![0]}">
+          <input class="slider__max-val" value="${values![1]}">` :
+          `<input class="slider-default_hidden"
+                  type="range" min="${ min }"
+                  max="${ max }"
+                  step="${ step }"
+                  value="${ value }">` }
 
         <output class="slider__display"></output>
         <div class="slider__scale slider__scale_${ type }">
           <div class="slider__range"></div>
           ${ range ?
-               `<div class="slider__roller slider__roller_first"></div>
-               <div class="slider__roller slider__roller_second"></div>`:
-               `<div class="slider__roller slider__roller_first"></div>` }
+            `<div class="slider__roller slider__roller_first"></div>
+            <div class="slider__roller slider__roller_second"></div>`:
+            `<div class="slider__roller slider__roller_first"></div>` }
         </div>
-      </div>
-    `; 
+      </div>`;
   }
 }
 
 
-export default Template;
-export type { ITemplate };
+export default ATemplate;
